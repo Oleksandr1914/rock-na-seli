01b0b8be89141b6e820eae3b02d1fddb4064dbe6
Oleksandr_Mochakov <sasha.mochakov@ukr.net>
2023-09-13 17:17:01 +0300

    changed plugins Gallerys

--- src/components/Gallery/Gallery.tsx
+++ src/components/Gallery/Gallery.tsx
@@ -7,10 +7,10 @@ import PhotoAlbum from "react-photo-album";
 import Lightbox from "yet-another-react-lightbox";
 import "yet-another-react-lightbox/styles.css";
 
-import Fullscreen from "yet-another-react-lightbox/plugins/fullscreen";
+
 import Slideshow from "yet-another-react-lightbox/plugins/slideshow";
 import Thumbnails from "yet-another-react-lightbox/plugins/thumbnails";
-import Zoom from "yet-another-react-lightbox/plugins/zoom";
+
 import "yet-another-react-lightbox/plugins/thumbnails.css";
 import NextJsImage from "./NextJsImage";
 
@@ -65,7 +65,7 @@ export default function Gallery({ children, year }: PGallery): JSX.Element {
                 index={index}
                 render={{ slide: NextJsImage }}
                 close={() => setIndex(-1)}
-                plugins={[Fullscreen, Slideshow, Thumbnails, Zoom]}
+                plugins={[Slideshow, Thumbnails]}
                 thumbnails={{ border: 0 }}
             />
         </>
