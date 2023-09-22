0da3f9458d3e280a2009e6e99f18d23eed98bbed
Oleksandr_Mochakov <sasha.mochakov@ukr.net>
2023-09-22 15:25:50 +0300

    added social media links

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -28,9 +28,26 @@ export default function Header(): JSX.Element {
                         <li><Links href='/group' > <SheetClose > ДЛЯ ГУРТІВ </SheetClose></Links></li>
                         <li><Links href='/gallery' > <SheetClose > ГАЛЕРЕЯ </SheetClose></Links></li>
                         <li><Links href='/contacts' > <SheetClose > КОНТАКТИ </SheetClose></Links></li>
-
                     </ul>
-
+                    <ul className="flex flex-col items-center gap-4 pt-20 bg-black "                >
+                        <li><a href="https://www.facebook.com/groups/173982049938109/user/100063204370759/"
+                            target="_blank"
+                            className="font-medium text-xl tracking-wider hover:text-hoverColor focus:text-activeLink hover:transition-all"
+                        ><SheetClose >Fasebook</SheetClose></a></li>
+                        <li><a href="https://www.instagram.com/rocknaseli/"
+                            target="_blank"
+                            className="font-medium text-xl tracking-wider hover:text-hoverColor focus:text-activeLink hover:transition-all"
+                        ><SheetClose >Instagram</SheetClose></a></li>
+                        <li><a href="https://www.youtube.com/@rock6351" target="_blank"
+                            className="font-medium text-xl tracking-wider hover:text-hoverColor focus:text-activeLink hover:transition-all"
+                        ><SheetClose >You Tube</SheetClose></a></li>
+                        <li><a href="mailto:rocknaseli@ukr.net"
+                            className="font-medium text-xl tracking-wider hover:text-hoverColor focus:text-activeLink hover:transition-all"
+                        ><SheetClose >rocknaseli@ukr.net</SheetClose></a></li>
+                        <li><a href="tel:+380673257403"
+                            className="font-medium text-xl tracking-wider hover:text-hoverColor focus:text-activeLink hover:transition-all"
+                        ><SheetClose >+38067-325-74-03</SheetClose></a></li>
+                    </ul>
                 </SheetContent>
             </Sheet>
             <ul className='hidden laptop:flex laptop:flex-row laptop:gap-7 laptop:ml-auto desktop:gap-10'>
